7e181f3788a738fd22954d2162c07f9769b77901
TaijiHiraoka <taiji308@gmail.com>
2020-11-23 21:00:17 +0900

    商品情報編集機能　修正２

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,6 +1,6 @@
 class ItemsController < ApplicationController
   before_action :set_item, only: [:show, :edit, :update]
-  before_action :move_to_index, except: [:edit]
+  before_action :move_to_index, only: [:edit]
 
   def index
     @items = Item.all.includes(:user).order("created_at DESC")
@@ -44,7 +44,7 @@ class ItemsController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in?
+    unless user_signed_in? && @item.user_id == current_user.id
       redirect_to action: :index
     end
   end
