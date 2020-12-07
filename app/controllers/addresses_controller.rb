c62242ecd9a6589a58f1d6fd007575f62816ed09
TaijiHiraoka <taiji308@gmail.com>
2020-12-07 23:26:05 +0900

    商品購入機能・修正

--- app/controllers/addresses_controller.rb
+++ app/controllers/addresses_controller.rb
@@ -27,8 +27,8 @@ class AddressesController < ApplicationController
   end
 
   def move_to_index
-    unless user_signed_in? && @item.user_id == current_user.id
-      redirect_to action: :index
+    unless user_signed_in? && @item.user_id != current_user.id
+      redirect_to root_path
     end
   end
 
